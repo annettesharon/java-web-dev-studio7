d2850e4bef0cfe7f74e0142de52abae2a1cafa31
annettesharon <annettesharon@ymail.com>
2022-09-19 19:55:57 -0500

    Commit interface and abstract class

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,10 +1,31 @@
 package org.launchcode.studio7;
 
-public class CD {
-
+public class CD extends BaseDisc implements OpticalDisc {
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
 
+    public CD(String name, double capacity, String contents, String discType) {
+        super(name, capacity, contents, discType);
+    }
+
+    @Override
+    public void spinDisc() {
+        System.out.println("The CD '" + this.name + "' spins at a rate of 200-500 rpm.");
+    }
+
+    @Override
+    public void writeData() {
+        System.out.println("A copy of the " + this.discType + " named '" + this.name + "' has been burned to the CD.");
+    }
+
+    @Override
+    public void readData() {
+        System.out.println("The CD is now playing the " + this.discType + " named '" + this.name + "'.");
+    }
+
+
 }
+
+
--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,10 +1,28 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements OpticalDisc {
 
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
+    public DVD(String name, double capacity, String contents, String discType) {
+        super(name, capacity, contents, discType);
+    }
+
+    @Override
+    public void spinDisc() {
+        System.out.println("The DVD '" + this.name + "' spins at a rate of 570-1600 rpm.");
+    }
+
+    @Override
+    public void writeData() {
+        System.out.println("A copy of the " + this.discType + " named '" + this.name + "' has been burned to the DVD.");
+    }
+
+    @Override
+    public void readData() {
+        System.out.println("The DVD is now playing the " + this.discType + " named '" + this.name + "'.");
+    }
 
 }
